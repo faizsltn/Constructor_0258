5eb6bc9c09a0d93bc696789e0cc7e8e42a70e022
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-05-13 11:16:42 +0700

    membuat class , akses modifier

--- chainfuntioncall.cpp
+++ chainfuntioncall.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 using namespace std;
 
+class buku
+{
+    string judul;
+    public:
+    buku setjadul(string jadul)
+    {
+        this->judul = judul;
+        return *this;  // chain function
+    }
+    string getjudul()
+    {
+        return this ->judul;
+    }
+};
