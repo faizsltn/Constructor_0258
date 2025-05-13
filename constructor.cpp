0f89edce1114489f7e9042cb6adc601269932f85
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-05-13 10:19:22 +0700

    membuat constructor

--- constructor.cpp
+++ constructor.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 
+class mahasiswa
+{
+    public :
+    mahasiswa(); //constructor
+};
+
+mahasiswa::mahasiswa()
+{
+    cout << "constructor terpanggil " << endl; // definisi constructor
+}
